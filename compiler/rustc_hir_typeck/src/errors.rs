ba357d1d2e3f29d5e369459975a187745eaa721a
Will Puhr <98563907+willpuhr@users.noreply.github.com>
2026-05-17 20:19:48 +0200

    Add error for `return` from const case

--- compiler/rustc_hir_typeck/src/errors.rs
+++ compiler/rustc_hir_typeck/src/errors.rs
@@ -109,6 +109,14 @@ pub(crate) struct QuestionMarkInConst {
 }
 
 #[derive(Diagnostic)]
+#[diag("cannot return from inside a `{$keyword}` initializer", code = E0807)]
+pub(crate) struct ReturnFromConst {
+    #[primary_span]
+    pub span: Span,
+    pub keyword: &'static str,
+}
+
+#[derive(Diagnostic)]
 #[diag("functions with the \"rust-call\" ABI must take a single non-self tuple argument")]
 pub(crate) struct RustCallIncorrectArgs {
     #[primary_span]
